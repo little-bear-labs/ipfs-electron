f70b2c73b8a2572bab8db700db14c18613a1dd6d
John Turpish <john@littlebearlabs.io>
2024-07-01 12:13:50 -0400

    list lines

--- build/spin.py
+++ build/spin.py
@@ -2,6 +2,7 @@
 
 from os      import chdir
 from os.path import dirname, isdir, isfile, join
+from sys     import stderr
 
 spin_dir = join(dirname(__file__), '..', '..', 'electron-spin')
 
@@ -11,7 +12,7 @@ else:
   exit(0)
 
 if not isfile('BUILD.gn'):
-  print('error: electron-spin directory present, but electron-spin/BUILD.gn absent. It is expected to define the target electron-spin')
+  print('error: electron-spin directory present, but electron-spin/BUILD.gn absent. It is expected to define the target electron-spin', file=stderr)
   exit(1)
 
 if isfile('builtin_schemes.h'):
